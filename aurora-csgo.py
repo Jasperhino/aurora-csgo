2b60a3e28ab2e5a6b2d1f5cc7b32465eca40024d
Jasper <blum.jasper@student.hpi.de>
2020-02-09 22:30:11 +0100

    Initial commit

---
+++ aurora-csgo.py
@@ -0,0 +1,9 @@
+import nanoleaf
+import time
+from nanoleaf import Aurora
+from gsi.server import GSIServer
+
+ip = "192.168.0.113"
+auth_token = "i1hxfiobrg2i6jhfbP8fs3rRfZol0umR"
+nl = Aurora(ip, auth_token)
+
